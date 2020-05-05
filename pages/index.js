e1cae63182306c00e716d0061070adcd4d84300a
millord <juancarlosmillord@gmail.com>
2020-05-05 17:56:30 -0400

    did my first fetching fromt the backend

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
 import axios from "axios";
+import React from "react";
 
 function Home() {
-  return <>home</>;
+  React.useEffect(() => {
+    getProducts();
+  }, []);
+
+  async function getProducts() {
+    const url = "http://localhost:3000/api/products";
+    const response = await axios.get(url);
+    console.log(response.data);
+  }
+
+  return <p>Home</p>;
 }
 
 export default Home;
