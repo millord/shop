8aa3b4f03686df63b5d5653bf331e3bd1dda866a
millord <juancarlosmillord@gmail.com>
2020-05-05 17:30:23 -0400

    fixed the active links issue

--- components/_App/Header.js
+++ components/_App/Header.js
@@ -1,13 +1,19 @@
 import { Menu, Container, Image, Icon } from "semantic-ui-react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 function Header() {
+  const router = useRouter();
+  function isActive(route) {
+    return route === router.pathname;
+  }
+
   const user = false;
   return (
     <Menu fluid id="menu" inverted>
       <Container text>
         <Link href="/">
-          <Menu.Item header>
+          <Menu.Item header active={isActive("/")}>
             <Image
               size="mini"
               src="/static/logo.svg"
@@ -17,14 +23,14 @@ function Header() {
           </Menu.Item>
         </Link>
         <Link href="/cart">
-          <Menu.Item header>
+          <Menu.Item header active={isActive("/cart")}>
             <Icon name="cart" size="large" />
             Cart
           </Menu.Item>
         </Link>
         {user && (
           <Link href="/create">
-            <Menu.Item header>
+            <Menu.Item header active={isActive("/create")}>
               <Icon name="add square" size="large" />
               Create
             </Menu.Item>
@@ -33,7 +39,7 @@ function Header() {
         {user ? (
           <>
             <Link href="/account">
-              <Menu.Item header>
+              <Menu.Item header active={isActive("/account")}>
                 <Icon name="user" size="large" />
                 Account
               </Menu.Item>
@@ -47,14 +53,14 @@ function Header() {
         ) : (
           <>
             <Link href="/login">
-              <Menu.Item header>
+              <Menu.Item header active={isActive("/login")}>
                 <Icon name="sign in" size="large" />
                 Login
               </Menu.Item>
             </Link>
 
             <Link href="/signup">
-              <Menu.Item header>
+              <Menu.Item header active={isActive("/signup")}>
                 <Icon name="signup" size="large" />
                 Signup
               </Menu.Item>
