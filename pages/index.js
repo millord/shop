87b899e8370d43554b9c65eb195871426aaf5c1c
millord <juancarlosmillord@gmail.com>
2020-05-05 20:02:52 -0400

    added the getInitialProps

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,15 @@
 import axios from "axios";
 import React from "react";
 
-function Home() {
-  React.useEffect(() => {
-    getProducts();
-  }, []);
-
-  async function getProducts() {
-    const url = "http://localhost:3000/api/products";
-    const response = await axios.get(url);
-    console.log(response.data);
-  }
-
+function Home(products) {
+  console.log(products);
   return <p>Home</p>;
 }
+
+Home.getInitialProps = async () => {
+  const url = "http://localhost:3000/api/products";
+  const response = await axios.get(url);
+  return { products: response.data };
+};
 
 export default Home;
